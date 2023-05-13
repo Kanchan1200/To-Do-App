b03f0b4b5fb6c4f69170cd7ffcd8d84366719595
kanchan1200 <kanchanc1200@gmail.com>
2023-05-13 14:53:12 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoWrapper from './Components/TodoWrapper'
+import './scss/style.scss'
+
+const App = () => {
+  return (
+    <div className='app'>
+        <TodoWrapper/>
+    </div>
+  )
+}
+
+export default App
